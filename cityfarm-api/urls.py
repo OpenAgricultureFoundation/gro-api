52376dcf15d7df95f0c53d54e2f38f9bcd457ec0
Leon Chambers <leonchambers@mit.edu>
2015-04-23 16:45:27 -0400

    Settings up settings

--- cityfarm-api/urls.py
+++ cityfarm-api/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'cityfarm_api.views.home', name='home'),
+    # url(r'^$', 'cityfarm-api.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
