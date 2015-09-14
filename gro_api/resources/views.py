a939f838fc449e95b5aa89fb1afb0d6ec15f8cc3
PlantOS <plantos@example.com>
2015-09-14 11:29:45 -0400

    Made resource views readable without auth

--- gro_api/resources/views.py
+++ gro_api/resources/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 from ..gro_api.permissions import EnforceReadOnly
 from .models import (
     ResourceType, ResourceProperty, ResourceEffect, Resource
@@ -14,7 +14,7 @@ class ResourceTypeViewSet(ModelViewSet):
     """ A type of resource, such as "air" or "water" """
     queryset = ResourceType.objects.all()
     serializer_class = ResourceTypeSerializer
-    permission_classes = [EnforceReadOnly, DjangoModelPermissions]
+    permission_classes = [EnforceReadOnly, DjangoModelPermissionsOrAnonReadOnly]
 
 
 class ResourcePropertyViewSet(ModelViewSet):
@@ -23,7 +23,7 @@ class ResourcePropertyViewSet(ModelViewSet):
     """
     queryset = ResourceProperty.objects.all()
     serializer_class = ResourcePropertySerializer
-    permission_classes = [EnforceReadOnly, DjangoModelPermissions]
+    permission_classes = [EnforceReadOnly, DjangoModelPermissionsOrAnonReadOnly]
 
 
 class ResourceEffectViewSet(ModelViewSet):
@@ -33,7 +33,7 @@ class ResourceEffectViewSet(ModelViewSet):
     """
     queryset = ResourceEffect.objects.all()
     serializer_class = ResourceEffectSerializer
-    permission_classes = [EnforceReadOnly, DjangoModelPermissions]
+    permission_classes = [EnforceReadOnly, DjangoModelPermissionsOrAnonReadOnly]
 
 
 class ResourceViewSet(ModelViewSet):
