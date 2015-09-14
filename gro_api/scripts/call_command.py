fdb877629cfd40a303c6bde791f6ee2f8eb9b250
Leon Chambers <leonchambers@mit.edu>
2015-09-13 23:58:07 -0400

    Switched to more general call_command script for all management commands

--- gro_api/scripts/call_command.py
+++ gro_api/scripts/call_command.py
@@ -1,17 +1,20 @@
+#!/usr/bin/env python
 import os
 import sys
-from itertools import chain
-from django.conf import settings
 from django.core.management import call_command
+from django.conf import settings
 from .load_env import load_env
 
-def graph_models():
+def runserver():
     settings_module = 'gro_api.gro_api.settings'
     os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
     load_env()
+    # Our settings file monkey patches django.setup, so we have to force django
+    # to load it before calling django.setup
+    settings.INSTALLED_APPS
     from django import setup
     setup()
-    call_command('graph_models', *sys.argv[1:])
+    call_command(*sys.argv[1:])
 
 if __name__ == '__main__':
-    graph_models()
+    runserver()
