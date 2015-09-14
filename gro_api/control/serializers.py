05873d1af5de2121e83f964bf0e2e14f20b7fdf1
Leon Chambers <leonchambers@mit.edu>
2015-09-13 23:42:57 -0400

    Authentication things

--- gro_api/control/serializers.py
+++ gro_api/control/serializers.py
@@ -1,13 +1,21 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Permission, Group
 from rest_framework.permissions import IsAdminUser
+from rest_framework.authtoken.models import Token
 from ..gro_api.serializers import BaseSerializer
 
 class UserSerializer(BaseSerializer):
     class Meta:
         model = get_user_model()
-        permission_classes = IsAdminUser
+
+class GroupSerializer(BaseSerializer):
+    class Meta:
+        model = Group
 
 class PermissionSerializer(BaseSerializer):
     class Meta:
         model = Permission
+
+class TokenSerializer(BaseSerializer):
+    class Meta:
+        model = Token
--- gro_api/control/views.py
+++ gro_api/control/views.py
@@ -6,24 +6,44 @@ This module defines a view function for each routine in
 
 from slugify import slugify
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Permission, Group
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAdminUser
 from .routines import Routine
-from .serializers import UserSerializer, PermissionSerializer
+from .serializers import (
+    UserSerializer, GroupSerializer, PermissionSerializer, TokenSerializer
+)
 
 
 class UserViewSet(ModelViewSet):
     """ A user registered with the system """
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAdminUser, ]
+
+
+class GroupViewSet(ModelViewSet):
+    """ A group of users assigned a set of permissions """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [IsAdminUser, ]
 
 
 class PermissionViewSet(ModelViewSet):
     """ A permission that can be given to or revoked from a user """
     queryset = Permission.objects.all()
     serializer_class = PermissionSerializer
+    permission_classes = [IsAdminUser, ]
+
+
+class TokenViewSet(ModelViewSet):
+    """ A token for logging in to the API """
+    queryset = Token.objects.all()
+    serializer_class = TokenSerializer
+    permission_classes = [IsAdminUser, ]
 
 
 all_views = {}
